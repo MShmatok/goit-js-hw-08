607963b9bbc284f70f5697ae7e7834bf1b8f346d
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-05-23 16:13:07 +0300

    3task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+const throttle = require('lodash.throttle');
 
+const ref = {
+    form: document.querySelector('.feedback-form'),
+}
+const LOCAL_FEEDBACK_KEY = 'feedback-form-state';
+const feedBackStorage = localStorage.getItem(LOCAL_FEEDBACK_KEY);
+
+checkInputForm(feedBackStorage);
+ref.form.addEventListener('input', throttle(onHandleFormInput, 500));
+ref.form.addEventListener('submit', onHandleSubmitForm);
+
+
+function onHandleFormInput(e) {
+    const feedBack = {
+        email: ref.form.email.value,
+        message: ref.form.message.value,
+    }
+    localStorage.setItem(LOCAL_FEEDBACK_KEY, JSON.stringify(feedBack));
+}
+
+function checkInputForm(feedBackStorage) {
+    if (feedBackStorage) {
+        const feedBack = JSON.parse(feedBackStorage);
+        ref.form.email.value = feedBack.email;
+        ref.form.message.value = feedBack.message;
+    }
+}
+
+function onHandleSubmitForm(e) {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem(LOCAL_FEEDBACK_KEY)));
+
+    localStorage.removeItem(LOCAL_FEEDBACK_KEY);
+    e.target.reset();
+}
